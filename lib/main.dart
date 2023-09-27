bdba996f5d5b3c56b644a7c205f789b0dc7aa89f
Mohammed <m97.chahboun@gmail.com>
2023-09-27 15:39:58 +0100

    update readme and update packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mc/mc.dart';
+import 'package:puzzle_hack/firebase_options.dart';
 import 'package:puzzle_hack/ui/home.dart';
 import 'package:puzzle_hack/utils/extensions.dart';
 import 'package:puzzle_hack/utils/theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         global.themeMode,
         () => MaterialApp(
               debugShowCheckedModeBanner: false,
-              title: 'Share Puzzle',              
+              title: 'Share Puzzle',
               themeMode: global.themeMode.v,
               darkTheme: darkTheme,
               theme: lightTheme,
